45c876e3e38fa7225be08838a07b30272550ee3e
Muhammad Ammad Hassan <67774874+AmmadDeveloper@users.noreply.github.com>
2022-08-12 03:37:42 +0500

    DB fixed in procfile

--- justCleanPrestige/settings.py
+++ justCleanPrestige/settings.py
@@ -101,41 +101,40 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'justCleanPrestige.wsgi.application'
-ASGI_APPLICATION='justCleanPrestige.asgi.application'
 
 
 
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
-}
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             "hosts": [('127.0.0.1', 6379)],
+#         },
+#     },
+# }
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     # 'default': {
-#     #     'ENGINE': 'django.db.backends.sqlite3',
-#     #     'NAME': BASE_DIR / 'db.sqlite3',
-#     # }
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'pickupclean',
-#         'HOST': '127.0.0.1',
-#         # 'HOST': '172.17.0.2',
-#         'USER': 'root',
-#         'PORT': '3306',
-#         # 'PASSWORD':'1234'
-#     },
-# }
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'pickupclean',
+    #     'HOST': '127.0.0.1',
+    #     # 'HOST': '172.17.0.2',
+    #     'USER': 'root',
+    #     'PORT': '3306',
+    #     # 'PASSWORD':'1234'
+    # },
+}
 
 import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
+prod_db  =  dj_database_url.config(conn_max_age=1500)
 DATABASES['default'].update(prod_db)
 
 
