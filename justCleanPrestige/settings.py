53cfc25fa07c1e0a56cb4fca6707f520b027cc64
Muhammad Ammad Hassan <67774874+AmmadDeveloper@users.noreply.github.com>
2022-08-12 03:03:01 +0500

    Whitenoise fixed

--- justCleanPrestige/settings.py
+++ justCleanPrestige/settings.py
@@ -193,7 +193,7 @@ STATICFILES_DIRS = (
     # os.path.join(PROJECT_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
