8597961935d56b2a783bae7db79e7e70d52c20d3
Muhammad Ammad Hassan <67774874+AmmadDeveloper@users.noreply.github.com>
2022-08-12 04:32:48 +0500

    DB fixed in procfile

--- justCleanPrestige/settings.py
+++ justCleanPrestige/settings.py
@@ -118,10 +118,10 @@ WSGI_APPLICATION = 'justCleanPrestige.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'pickupclean',
@@ -133,11 +133,11 @@ DATABASES = {
     # },
 }
 
-import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=1500)
-DATABASES['default'].update(prod_db)
-
-
+# import dj_database_url
+# prod_db  =  dj_database_url.config(conn_max_age=1500)
+# DATABASES['default'].update(prod_db)
+#
+#
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
